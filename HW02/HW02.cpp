bdd11a8643a0403b42ccbceb3c5189081118c144
abrar alhussein <sci_223586@omu.edu.ly>
2024-08-14 23:26:17 +0200

    upload file HW02

---
+++ HW02/HW02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int  studentID;
+    string name;
+
+cout<<"Enter your name\n";
+getline(cin,name);
+cout<<"Enter your ID\n";
+cin>>studentID;
+cout<<"name: "<<name<<endl<<"student ID:"<<studentID<<endl;
+
+
+
+    return 0;
+}
